8f72facc0a5d09d51d07ba58b00e63b23aaa5749
Camleaf <bdaace09@gmail.com>
2026-02-06 19:17:00 -0500

    fix

--- lua/user/esp-clangd.lua
+++ lua/user/esp-clangd.lua
@@ -1,14 +1,4 @@
 local m = {}
--- Find the latest ESP Clang installation
-local function find_esp_clang()
-    local glob = vim.fn.glob("~/.espressif/tools/esp-clang/esp-20.1.1_20250829/esp-clang/bin/clangd", 1, 1)
-    if #glob == 0 then
-        vim.notify("ESP Clang not found", vim.log.levels.ERROR)
-        return nil
-    end
-    table.sort(glob)
-    return glob[#glob] .. "/clangd"  -- the clangd executable
-end
 
 function m.load()
     -- Setup the ESP Clang LSP as its own server
@@ -17,14 +7,16 @@ function m.load()
         vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
             pattern = { "*.c", "*.cpp", "*.h", "*.hpp" },
             callback = function()
-                vim.lsp.start({
-                    cmd = {vim.fn.expand(clangd_path), '--query-driver=' .. vim.fn.expand("~") .. "/.espressif/tools/**/bin/*"},
-                    filetypes = { "c", "cpp", "objc", "objcpp" },
-                    root_dir = vim.fs.root(0,{"sdkconfig"}),
-                    on_attach = function()
-                        vim.notify("ESP Clang LSP attached!", vim.log.levels.INFO)
-                    end,
-                })
+                if vim.fs.root(0,{"sdkconfig"}) ~= nil then
+                    vim.lsp.start({
+                        cmd = {vim.fn.expand(clangd_path), '--query-driver=' .. vim.fn.expand("~") .. "/.espressif/tools/**/bin/*"},
+                        filetypes = { "c", "cpp", "objc", "objcpp" },
+                        root_dir = vim.fs.root(0,{"sdkconfig"}),
+                        on_attach = function()
+                            vim.notify("ESP Clang LSP attached!", vim.log.levels.INFO)
+                        end,
+                    })
+                end
             end,
         })
     else
--- lua/user/lspSetup.lua
+++ lua/user/lspSetup.lua
@@ -10,6 +10,7 @@ function module.load()
                     globals = { "vim" }}}}})
 
     vim.lsp.config("arduino_language_server",{
+        filetypes = {"c","cpp","arduino"},
         cmd = {
             "arduino-language-server",
             "-cli-config", string.format("/home/%s/.arduino15/arduino-cli.yaml",Name),
