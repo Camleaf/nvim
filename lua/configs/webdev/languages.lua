8e0edbd71e84d88a17b6f8dbe2454dec8f1218eb
Camleaf <bdaace09@gmail.com>
2026-04-23 17:59:47 -0400

    cmt

--- lua/configs/webdev/languages.lua
+++ lua/configs/webdev/languages.lua
@@ -35,7 +35,7 @@ end
 function module.getLspNames()
     return {
      "lua_ls", "cssls", "basedpyright", "ts_ls","html","tailwindcss",
-     "bashls", "json"
+     "bashls", "jsonls"
     }
 end
 
--- lua/configs/webdev/lspSetup.lua
+++ lua/configs/webdev/lspSetup.lua
@@ -2,6 +2,13 @@ local module = {}
 
 function module.load()
     -- Needed for compilation but nothing special here for moment
+   vim.filetype.add({
+      extension = {
+        mdx = "markdown.mdx",
+      },
+      filename = {},
+      pattern = {},
+    })
 end
 
 
